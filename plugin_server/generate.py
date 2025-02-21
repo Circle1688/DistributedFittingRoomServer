8ef34521d738ccc1aa232f97d1e4d00c1146e664
Circle1688 <986638435@qq.com>
2025-02-22 03:12:50 +0800

    Update generate.py

--- plugin_server/generate.py
+++ plugin_server/generate.py
@@ -45,83 +45,87 @@ def update_task_status(user_id, task_id, status):
 
 
 def generate_process(task_id, args):
-    result = False
-    user_id = args["user_id"]
+    try:
+        result = False
+        user_id = args["user_id"]
 
-    # 更新任务状态
-    update_task_status(user_id, task_id, "STARTED")
+        # 更新任务状态
+        update_task_status(user_id, task_id, "STARTED")
 
-    start_time = time.time()
-    server_logger.info(f"[{task_id}] Start process...")
+        start_time = time.time()
+        server_logger.info(f"[{task_id}] Start process...")
 
-    # 输出路径
-    output_path = TEMP_OUTPUT_DIR
+        # 输出路径
+        output_path = TEMP_OUTPUT_DIR
 
-    # 清除输出目录
-    clear_folder(output_path)
+        # 清除输出目录
+        clear_folder(output_path)
 
-    task_type = args['task_type']
-    if task_type == "upscale":
-        video_url = args["data"]["video_url"]
-        # 下载视频
-        input_path = download_file(video_url, download_dir=TEMP_DOWNLOAD_DIR)
+        task_type = args['task_type']
+        if task_type == "upscale":
+            video_url = args["data"]["video_url"]
+            # 下载视频
+            input_path = download_file(video_url, download_dir=TEMP_DOWNLOAD_DIR)
 
-        video_output_path = os.path.join(output_path, f'{task_id}_upscale.mp4')
+            video_output_path = os.path.join(output_path, f'{task_id}_upscale.mp4')
 
-        if upscale_process(input_path, video_output_path):
-            # 生成视频缩略图
-            extract_video_cover(video_output_path)
-            # 上传到oss
-            result = upload_files_oss(output_path, user_id)
+            if upscale_process(input_path, video_output_path):
+                # 生成视频缩略图
+                extract_video_cover(video_output_path)
+                # 上传到oss
+                result = upload_files_oss(output_path, user_id)
 
-    else:
-        # 下载头像
-        source_image_path = download_avatar(user_id)
+        else:
+            # 下载头像
+            source_image_path = download_avatar(user_id)
 
-        # 头像获取成功
-        if source_image_path:
-            request_data = args["data"]
+            # 头像获取成功
+            if source_image_path:
+                request_data = args["data"]
 
-            server_logger.info("UE...")
-            # UE生成图像
-            images_folder = ue_process(request_data)
+                server_logger.info("UE...")
+                # UE生成图像
+                images_folder = ue_process(request_data)
 
-            if task_type == "image":
-                server_logger.info("FaceFusion image...")
-                # facefusion图像
-                if facefusion_image(task_id, source_image_path, images_folder, output_path,
-                                    request_data['image_options']):
-                    # 上传到oss
-                    result = upload_files_oss(output_path, user_id)
+                if task_type == "image":
+                    server_logger.info("FaceFusion image...")
+                    # facefusion图像
+                    if facefusion_image(task_id, source_image_path, images_folder, output_path,
+                                        request_data['image_options']):
+                        # 上传到oss
+                        result = upload_files_oss(output_path, user_id)
 
-            elif task_type == "video":
-                target_image_path = find_png_files(images_folder)[0]
+                elif task_type == "video":
+                    target_image_path = find_png_files(images_folder)[0]
 
-                image_output_path = os.path.join(TEMP_DIR, 'temp.png')
+                    image_output_path = os.path.join(TEMP_DIR, 'temp.png')
 
-                server_logger.info("Pre swap face...")
-                # 首次换脸
-                first_result, image_output_path = facefusion_image_internal(source_image_path, target_image_path,
-                                                                            image_output_path)
-                # 首次换脸成功
-                if first_result:
-                    server_logger.info("PixVerse...")
-                    # 处理视频
-                    video_path = os.path.join(TEMP_DIR, 'temp.mp4')
-                    if pixverse_process(image_output_path, video_path, request_data['video_options']):
-                        server_logger.info("Process video...")
+                    server_logger.info("Pre swap face...")
+                    # 首次换脸
+                    first_result, image_output_path = facefusion_image_internal(source_image_path, target_image_path,
+                                                                                image_output_path)
+                    # 首次换脸成功
+                    if first_result:
+                        server_logger.info("PixVerse...")
+                        # 处理视频
+                        video_path = os.path.join(TEMP_DIR, 'temp.mp4')
+                        if pixverse_process(image_output_path, video_path, request_data['video_options']):
+                            server_logger.info("Process video...")
 
-                        # 视频换脸
-                        video_output_path = facefusion_video(task_id, source_image_path, video_path, output_path)
-                        if video_output_path:
-                            # 生成视频缩略图
-                            extract_video_cover(video_output_path)
-                            # 上传到oss
-                            result = upload_files_oss(output_path, user_id)
+                            # 视频换脸
+                            video_output_path = facefusion_video(task_id, source_image_path, video_path, output_path)
+                            if video_output_path:
+                                # 生成视频缩略图
+                                extract_video_cover(video_output_path)
+                                # 上传到oss
+                                result = upload_files_oss(output_path, user_id)
 
-    end_time = round(time.time() - start_time, 2)
-    server_logger.info(f"[{task_id}] Finish process in {end_time} seconds.")
+        end_time = round(time.time() - start_time, 2)
+        server_logger.info(f"[{task_id}] Finish process in {end_time} seconds.")
 
-    update_task_status(user_id, task_id, "SUCCESS" if result else "FAILED")
+        update_task_status(user_id, task_id, "SUCCESS" if result else "FAILED")
 
-    return result
+        return result
+    except Exception as e:
+        server_logger.info(f"[Generate] Error: {e}")
+        return False
