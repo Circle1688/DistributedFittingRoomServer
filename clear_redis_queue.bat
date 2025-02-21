6c24b8266725a4a20edfc6bc1ac16df82fcc4828
Circle1688 <986638435@qq.com>
2025-02-21 16:31:24 +0800

    Update clear_redis_queue.bat

--- clear_redis_queue.bat
+++ clear_redis_queue.bat
@@ -2,3 +2,10 @@
 cd /d "%~dp0"
 cd Redis/
 redis-cli.exe FLUSHALL
+
+if %errorlevel% equ 0 (
+    echo FLUSHALL command executed successfully.
+) else (
+    echo Failed to execute FLUSHALL command.
+)
+pause
