8aff2332710d4ec4378b3d7455248d7912c70fde
Circle1688 <986638435@qq.com>
2025-02-22 17:04:46 +0800

    Update worker.bat

--- worker.bat
+++ worker.bat
@@ -3,4 +3,5 @@ title worker
 cd /d "%~dp0"
 set CELERY=%~dp0.venv\Scripts\celery.exe
 
-%CELERY% -A faceswap worker -P gevent -Q high_priority,low_priority -l INFO --concurrency=1
+rem %CELERY% -A faceswap worker -P gevent -Q high_priority,low_priority -l INFO --concurrency=1
+%CELERY% -A faceswap worker -P gevent -l INFO
