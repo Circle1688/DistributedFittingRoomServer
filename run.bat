b7656a6d01df268e6140d7c5feaac3f2983841ff
Circle1688 <986638435@qq.com>
2025-02-23 11:36:41 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,10 +3,10 @@ title fittingroomserver
 cd /d "%~dp0"
 
 REM Start redis
-start %~dp0run_redis.bat
+start /MIN %~dp0run_redis.bat
 
 REM Start task server
-start %~dp0run_task.bat
+start /MIN %~dp0run_task.bat
 
 REM Clear redis queue
 start %~dp0clear_redis_queue.bat
