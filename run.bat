e67f9d6fd9772809f2567c595aae986cfd0805a6
Circle1688 <986638435@qq.com>
2025-02-23 11:30:01 +0800

    update

--- run.bat
+++ run.bat
@@ -2,11 +2,14 @@
 title fittingroomserver
 cd /d "%~dp0"
 
+REM Start redis
+start %~dp0run_redis.bat
+
 REM Start task server
 start %~dp0run_task.bat
 
-REM Start redis
-start %~dp0run_redis.bat
+REM Clear redis queue
+start %~dp0clear_redis_queue.bat
 
 REM Start fitting room server
 set PYTHON=%~dp0.venv\Scripts\python.exe
